f5e5ad17c9ee3061d745d09465fbb44866840a2a
stormytuna <stormytuna@outlook.com>
2025-12-21 17:31:37 +0000

    add prosthetic limb

--- src/main/java/morerelics/MoreRelics.java
+++ src/main/java/morerelics/MoreRelics.java
@@ -17,6 +17,7 @@ import morerelics.relics.LineGraph;
 import morerelics.relics.Lubricant;
 import morerelics.relics.MoldedSand;
 import morerelics.relics.MummifiedFoot;
+import morerelics.relics.ProstheticLimb;
 import morerelics.relics.PuritySeal;
 import morerelics.relics.StickyHand;
 import morerelics.relics.StuffedBird;
@@ -107,6 +108,7 @@ public class MoreRelics implements
         BaseMod.addRelic(new StuffedBird(), RelicType.SHARED);
         BaseMod.addRelic(new PuritySeal(), RelicType.SHARED);
         BaseMod.addRelic(new TinBracelet(), RelicType.SHARED);
+        BaseMod.addRelic(new ProstheticLimb(), RelicType.SHARED);
         BaseMod.addRelic(new HarmonizationMatrix(), RelicType.BLUE);
         BaseMod.addRelic(new Lubricant(), RelicType.BLUE);
         BaseMod.addRelic(new BeckoningSnecko(), RelicType.PURPLE);
---
+++ src/main/java/morerelics/relics/ProstheticLimb.java
@@ -0,0 +1,36 @@
+package morerelics.relics;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import basemod.BaseMod;
+import morerelics.MoreRelics;
+
+public class ProstheticLimb extends BaseRelic {
+    public static final String ID = MoreRelics.makeID("ProstheticLimb");
+    public static final RelicTier TIER = RelicTier.BOSS;
+    public static final LandingSound LANDING_SOUND = LandingSound.HEAVY;
+    public static final int DRAW = 1;
+    public static final int EXTRA_HAND_SIZE = 1;
+
+    public ProstheticLimb() {
+        super(ID, TIER, LANDING_SOUND);
+    }
+
+    public AbstractRelic makeCopy() {
+        return new ProstheticLimb();
+    }
+
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + DRAW + DESCRIPTIONS[1] + EXTRA_HAND_SIZE + DESCRIPTIONS[2];
+    }
+
+    public void atTurnStart() {
+        flash();
+        addToTop(new DrawCardAction(DRAW));
+    }
+
+    public void onEquip() {
+        BaseMod.MAX_HAND_SIZE += EXTRA_HAND_SIZE;
+    }
+}
